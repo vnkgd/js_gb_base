2ede32e1d187052c8f8e217ea8f8d1541bc5eabb
vnkgd <vnkgd@mail.ru>
2024-04-04 19:37:53 +0200

    Основы JavaScript

---
+++ ht2/script.js
@@ -0,0 +1,12 @@
+// 1
+let tempCel = 100;
+let tempFar = (9 / 5) * tempCel + 32;
+// console.log(tempCel);
+console.log(`Температура в градусах Цельсия: ${tempCel} 
+Температура по Фаренгейту: ${tempFar}`);
+
+// 2
+let name = 'Федя';
+let admin = name;
+// console.log(name);
+console.log(admin);
--- seminar1/script.js
+++ seminar1/script.js
@@ -1,19 +1,63 @@
-let num = 123;
-console.log(num);
-//переменная а
-let a = 5;
-a = 7;
-console.log(a);
-let b = 1 + 2 + 3;
-/* выводим в консоль 
-значение переменно b */
-console.log(b);
-
-let c = 10;
-let d = 2;
-let sum = c + d;
-console.log(sum);
-
-
-
+// 1
 // console.log('Текст из script.js');
+
+// 2
+// let num = 123;
+// console.log(num);
+// //переменная а
+// let a = 5;
+// a = 7;
+// console.log(a);
+// let b = 1 + 2 + 3;
+// /* выводим в консоль
+// значение переменно b */
+// console.log(b);
+
+// 3
+// let c = 10;
+// let d = 2;
+// let sum = c + d;
+// console.log(sum);
+// let dif = c - d;
+// console.log(dif);
+// let div = c / d;
+// console.log(div);
+// let mult = c * d;
+// console.log(mult);
+// let all = c + d + sum + dif + div + mult;
+// console.log(all);
+// // либо сразу так
+// console.log(c + d + sum + dif + div + mult);
+
+
+// 4
+//5
+
+// let a = 1.5;
+// let b = 0.75;
+// console.log(a + b);
+
+// let a = -5;
+// a = -a;
+// console.log(a);
+
+// //6
+// let name = "Иван";
+// let lastname = "Петров";
+// console.log(`Привет ${name} ${lastname}`);
+// // Либо так:
+// console.log(name + ' ' + lastname);
+
+// let code = "Java";
+// let code2 = "Script";
+// console.log(`Ты изучаешь ${code}${code2}?`);
+// // Либо так:
+// console.log('Java' + 'Script');
+
+// let letter1 = "Hello";
+// let letter2 = "World";
+// console.log(`Твой первый код: ${letter1} ${letter2}!`);
+// // Либо так:
+// console.log('Hello' + ' ' + 'World');
+// // Либо так:
+// console.log('Hello ' + 'World');
