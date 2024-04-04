45ee6b265114bf0a956606e48ff9d56a2fd227b4
vnkgd <vnkgd@mail.ru>
2024-04-04 18:49:45 +0200

    Основы JavaScript

---
+++ seminar1/script.js
@@ -0,0 +1,19 @@
+let num = 123;
+console.log(num);
+//переменная а
+let a = 5;
+a = 7;
+console.log(a);
+let b = 1 + 2 + 3;
+/* выводим в консоль 
+значение переменно b */
+console.log(b);
+
+let c = 10;
+let d = 2;
+let sum = c + d;
+console.log(sum);
+
+
+
+// console.log('Текст из script.js');
